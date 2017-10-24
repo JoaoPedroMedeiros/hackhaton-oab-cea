8a2120e00bc0e09c1962b1e0a90cba1b696b3cb3
Antonio Carlos Lázaro de Oliveira <contatompx@hotmail.com>
2017-10-23 22:05:33 -0200

    Admin PHP - backend

---
+++ cea_admin/application/views/admin/_templates/main_sidebar.php
@@ -0,0 +1,89 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+?>
+
+            <aside class="main-sidebar">
+                <section class="sidebar">
+<?php if ($admin_prefs['user_panel'] == TRUE): ?>
+                    <!-- Sidebar user panel -->
+                    <div class="user-panel">
+                        <div class="pull-left image">
+                            <img src="<?php echo base_url($avatar_dir . '/m_001.png'); ?>" class="img-circle" alt="User Image">
+                        </div>
+                        <div class="pull-left info">
+                            <p><?php echo $user_login['firstname'].$user_login['lastname']; ?></p>
+                            <a href="#"><i class="fa fa-circle text-success"></i> <?php echo lang('menu_online'); ?></a>
+                        </div>
+                    </div>
+
+<?php endif; ?>
+
+                    <!-- Sidebar menu -->
+                    <ul class="sidebar-menu">
+                        
+
+                        <li class="header text-uppercase"><?php echo lang('menu_main_navigation'); ?></li>
+                        
+                        <li class="<?=active_link_controller('dashboard')?>">
+                            <a href="<?php echo site_url('admin/dashboard'); ?>">
+                                <i class="fa fa-dashboard"></i> <span><?php echo lang('menu_dashboard'); ?></span>
+                            </a>
+                        </li>
+
+                        <li class="header text-uppercase">ATENDIMENTO</li>
+                        
+                        <li class="treeview <?=active_link_controller('users')?>">
+                            <a href="#">
+                                <i class="fa fa-cogs"></i>
+                                <span>Ouvidoria</span>
+                                <i class="fa fa-angle-left pull-right"></i>
+                            </a>
+                            <ul class="treeview-menu">
+                                <li class="<?=active_link_function('interfaces')?>"><a href="<?php echo site_url('admin/prefs/interfaces/admin'); ?>">Listagem</a></li>
+                            </ul>
+                            <ul class="treeview-menu">
+                                <li class="<?=active_link_function('interfaces')?>"><a href="<?php echo site_url('admin/prefs/interfaces/admin'); ?>">Arquivos</a></li>
+                            </ul>
+                        </li>
+                        
+                        <li class="header text-uppercase"><?php echo lang('menu_administration'); ?></li>
+                        <li class="<?=active_link_controller('users')?>">
+                            <a href="<?php echo site_url('admin/users'); ?>">
+                                <i class="fa fa-user"></i> <span><?php echo lang('menu_users'); ?></span>
+                            </a>
+                        </li>
+                        <li class="<?=active_link_controller('groups')?>">
+                            <a href="<?php echo site_url('admin/groups'); ?>">
+                                <i class="fa fa-shield"></i> <span><?php echo lang('menu_security_groups'); ?></span>
+                            </a>
+                        </li>
+                        <li class="treeview <?=active_link_controller('prefs')?>">
+                            <a href="#">
+                                <i class="fa fa-cogs"></i>
+                                <span><?php echo lang('menu_preferences'); ?></span>
+                                <i class="fa fa-angle-left pull-right"></i>
+                            </a>
+                            <ul class="treeview-menu">
+                                <li class="<?=active_link_function('interfaces')?>"><a href="<?php echo site_url('admin/prefs/interfaces/admin'); ?>"><?php echo lang('menu_interfaces'); ?></a></li>
+                            </ul>
+                        </li>
+                        <li class="<?=active_link_controller('files')?>">
+                            <a href="<?php echo site_url('admin/files'); ?>">
+                                <i class="fa fa-file"></i> <span><?php echo lang('menu_files'); ?></span>
+                            </a>
+                        </li>
+                        <li class="<?=active_link_controller('database')?>">
+                            <a href="<?php echo site_url('admin/database'); ?>">
+                                <i class="fa fa-database"></i> <span><?php echo lang('menu_database_utility'); ?></span>
+                            </a>
+                        </li>
+
+                        <li class="<?=active_link_controller('resources')?>">
+                            <a href="<?php echo site_url('admin/resources'); ?>">
+                                <i class="fa fa-cubes"></i> <span><?php echo lang('menu_resources'); ?></span>
+                            </a>
+                        </li>
+                    </ul>
+                </section>
+            </aside>
---
+++ cea_admin/application/views/admin/resources/index.php
@@ -0,0 +1,29 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+?>
+
+            <div class="content-wrapper">
+                <section class="content-header">
+                    <?php echo $pagetitle; ?>
+                    <?php echo $breadcrumb; ?>
+                </section>
+
+                <section class="content">
+                    <div class="row">
+                        <div class="col-md-12">
+                             <div class="box">
+                                <div class="box-header with-border">
+                                    <h3 class="box-title">Exemplo de Box padrão</h3>
+                                    <p>Para criar um novo item de menu, copie o model, view e controller deste, e adicione o link no main_sidebar</p>
+                                </div>
+                                <div class="box-body">
+
+
+
+                                </div>
+                            </div>
+                         </div>
+                    </div>
+                </section>
+            </div>
