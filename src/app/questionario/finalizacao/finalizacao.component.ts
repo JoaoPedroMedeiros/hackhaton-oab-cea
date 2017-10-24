0e0a39c560a9d9a85df251a6490d63cca8aa97ef
João Pedro Medeiros de Farias <joaopmedeirosfarias@gmail.com>
2017-10-23 20:14:02 -0700

    Geração da petição

--- src/app/questionario/finalizacao/finalizacao.component.ts
+++ src/app/questionario/finalizacao/finalizacao.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { PerguntaComponent } from '../pergunta-component-custom';
 import { RespostaService } from '../service/resposta.service';
 import { ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
-import * as jsPDF from 'jspdf';
 
 @Component({
   selector: 'app-finalizacao',
@@ -19,34 +18,7 @@ export class FinalizacaoComponent extends PerguntaComponent {
     return true;
   }
 
-  onChange(event) {
-    console.log(event);
-    var files = event.srcElement.files;
-    console.log(files);
-
-    console.log(files);
-  }
-
-  pdf() {
-    // var doc = new jsPDF();
-    // doc.text(20, 20, 'Hello world!');
-    // doc.text(20, 30, 'This is client-side Javascript, pumping out a PDF.');
-    // doc.addPage();
-    // doc.text(20, 20, 'Do you like that?');
-
-    // // Save the PDF
-    // doc.save('Test.pdf');
-
-    // var doc = new jsPDF();
-    
-    // doc.setFontSize(40);
-    // doc.text(35, 25, 'Paranyan loves jsPDF');
-    // doc.addImage(imgData, 'JPEG', 15, 40, 180, 160);
-
-    // doc.save('test.pdf'); 
-  }
-
-  titulo(): string {
+  titulo(): string {  
     return '';
   }
   descricao(): string {
--- src/app/questionario/peticao/peticao.component.ts
+++ src/app/questionario/peticao/peticao.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { PerguntaComponent } from '../pergunta-component-custom';
 import { RespostaService } from '../service/resposta.service';
 import { ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import * as jsPDF from 'jspdf';
+import { Respostas } from '../model/Respostas';
 
 @Component({
   selector: 'app-peticao',
@@ -10,9 +12,39 @@ import { ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 })
 export class PeticaoComponent extends PerguntaComponent {
 
+  respostas: Respostas;
+
+  assunto: string;
+
   constructor(protected respostaService: RespostaService) {
     super(respostaService);
+
+    this.respostas = respostaService.getRespostas();
+    /*
+    * 1 - Cobraram o que já foi pago
+    * 2 - Cobraram uma taxa que não contratei
+    * 3 - Cobraram por um serviço que nunca requisitei/utilizei
+    * 4 - Cobraram um valor maior do que eu contratei
+    */
+    this.assunto = '';
+    if (this.respostas.problema = 1)
+      this.assunto = 'Cobraram o que já foi pago';
+    if (this.respostas.problema = 2)
+      this.assunto = 'Cobraram uma taxa que não contratei';
+    if (this.respostas.problema = 3)
+      this.assunto = 'Cobraram por um serviço que nunca requisitei/utilizei';
+    if (this.respostas.problema = 4)
+      this.assunto = 'Cobraram um valor maior do que eu contratei';
   }
+
+  pdf() {
+    var pdf = new jsPDF('p','pt','a4');
+    pdf.addHTML(document.getElementById('divPeticao'), 0, 0, {
+      pagesplit: true
+    }, function() {
+            pdf.save('web.pdf');
+        });
+      }
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
     return true;
